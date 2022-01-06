597b103f848ef0f842882c91daf60edc6cec5e73
Andrei Navoichyk <andrei@cere.io>
2022-01-06 13:08:44 +0300

    set highest pieces count

--- bin/node/runtime/src/lib.rs
+++ bin/node/runtime/src/lib.rs
@@ -432,7 +432,7 @@ pallet_staking_reward_curve::build! {
 		max_inflation: 0_013_000,
 		ideal_stake: 0_100_000,
 		falloff: 0_050_000,
-		max_piece_count: 40,
+		max_piece_count: 1000,
 		test_precision: 0_005_000,
 	);
 }
