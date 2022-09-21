e4c5eb568142ef128efcbcc3a472c40559a1c2e1
Andrei Navoichyk <andrei@cere.io>
2022-09-21 14:26:05 +0400

    updated code_len

--- frame/contracts/src/tests.rs
+++ frame/contracts/src/tests.rs
@@ -251,7 +251,6 @@ parameter_types! {
 	pub const MaxValueSize: u32 = 16_384;
 	pub const DeletionQueueDepth: u32 = 1024;
 	pub const DeletionWeightLimit: Weight = 500_000_000_000;
-	pub const MaxCodeSize: u32 = 160 * 1024;
 	pub MySchedule: Schedule<Test> = <Schedule<Test>>::default();
 	pub const TransactionByteFee: u64 = 0;
 }
