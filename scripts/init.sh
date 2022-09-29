60774a6f6d6f9a31916a53d53120d5fa994a1b57
Andrei Navoichyk <andrei@cere.io>
2022-09-30 00:15:12 +0400

    update rust version to 2021-11-29

--- scripts/init.sh
+++ scripts/init.sh
@@ -4,6 +4,6 @@ set -e
 
 echo "*** Initializing WASM build environment"
 
-rustup install nightly-2021-11-01
+rustup install nightly-2021-11-29
 
-rustup target add wasm32-unknown-unknown --toolchain nightly-2021-11-01
+rustup target add wasm32-unknown-unknown --toolchain nightly-2021-11-29
