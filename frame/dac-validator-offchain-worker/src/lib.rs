15b8341ac1929dfc067bc8fdb6378fb77d3f0dc3
Maksim Ramanenkau <cbnhrpo@gmail.com>
2023-03-01 09:33:18 +0100

    Create OCW, read from redis, compare bytes sent and bytes recieved, send tx onchain with the comparison
    Make http request from ocw
    
    Fix arbitary http request
    
    Try to connect to redis
    
    Clean up
    
    Try use redis via webdis
    
    Introduce structs
    
    Small improvements
    
    Code quality improvements
    
    Call offchain validator once per era
    
    Set shorted duration
    
    Update EPOCH_DURATION_IN_BLOCKS
    
    Fix short durations
    
    Integrade OCW to staking
    
    Specify ValidatorId type in OCW config trait bound
    
    Add logs
    
    Try fix issue with storage
    
    Increase points via on_initialize hook
    
    Try to send tx from offchain worker
    
    Read and store data from OCW
    
    Configure keys for OCW
    
    Use more advanced data storage
    
    Make save_validated_data with updated storage
    
    Revert "Fix short durations"
    
    This reverts commit d36efd54594d2c698e7c713d85120c6c5b86e9ca.
    
    Revert "Update EPOCH_DURATION_IN_BLOCKS"
    
    This reverts commit 87c67b138f3f593fffd5b745a4261c682dd0a24a.
    
    Revert "Set shorted duration"
    
    This reverts commit e5283c8202308364d32418ff4c483ef0b9996f0f.
    
    Fix logs for --chain=local
    
    Add signer to validation results and fix bug with KeyTypeId
    
    Validated fetched data and send tx
    
    Migrate to the latest pallet macro
    
    Remove ocw with old pallet macro

---
+++ frame/dac-validator-offchain-worker/src/lib.rs
@@ -0,0 +1,360 @@
+#![cfg_attr(not(feature = "std"), no_std)]
+
+use alloc::{format, string::String};
+use alt_serde::{de::DeserializeOwned, Deserialize, Serialize};
+use codec::{Encode, Decode, MaxEncodedLen, HasCompact};
+use frame_support::{
+    decl_event, decl_module, decl_storage,
+    log::{error, info, warn},
+    traits::Currency,
+    weights::{Weight},
+    dispatch::DispatchResult,
+    RuntimeDebug,
+    BoundedVec,
+    parameter_types,
+};
+use frame_system::{ensure_signed, offchain::{CreateSignedTransaction, Signer, SigningTypes, AppCrypto, SendSignedTransaction}};
+use pallet_staking::{self as staking};
+use sp_core::crypto::{KeyTypeId, UncheckedFrom};use sp_io::offchain::timestamp;
+use sp_runtime::offchain::{http, Duration};
+use sp_std::{vec, vec::Vec};
+extern crate alloc;
+use pallet_session as session;
+
+parameter_types! {
+	pub DdcValidatorsQuorumSize: u32 = 3;
+}
+
+use core::fmt::Debug;
+use scale_info::TypeInfo;
+
+type BalanceOf<T> = <<T as pallet_contracts::Config>::Currency as Currency<
+    <T as frame_system::Config>::AccountId,
+>>::Balance;
+
+pub const KEY_TYPE: KeyTypeId = KeyTypeId(*b"dacv");
+
+pub const HTTP_TIMEOUT_MS: u64 = 30_000;
+
+const TIME_START_MS: u64 = 1_672_531_200_000;
+const ERA_DURATION_MS: u64 = 120_000;
+const ERA_IN_BLOCKS: u8 = 20;
+
+const DATA_PROVIDER_URL: &str = "http://localhost:7379/";
+
+type ResultStr<T> = Result<T, &'static str>;
+
+#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[serde(crate = "alt_serde")]
+#[serde(rename_all = "camelCase")]
+pub struct RedisFtAggregate {
+    #[serde(rename = "FT.AGGREGATE")]
+    pub ft_aggregate: (u32, Vec<String>),
+}
+
+#[derive(Clone)]
+struct BytesSent {
+    node_public_key: String,
+    era: String,
+    sum: u32,
+}
+
+impl BytesSent {
+    pub fn new(aggregate: RedisFtAggregate) -> BytesSent {
+        let (_, values) = aggregate.ft_aggregate;
+
+        BytesSent {
+            node_public_key: values[1].clone(),
+            era: values[3].clone(),
+            sum: values[5].parse::<u32>().expect("bytesSentSum must be convertable to u32"),
+        }
+    }
+}
+
+#[derive(Clone)]
+struct BytesReceived {
+    node_public_key: String,
+    era: String,
+    sum: u32,
+}
+
+impl BytesReceived {
+    pub fn new(aggregate: RedisFtAggregate) -> BytesReceived {
+        let (_, values) = aggregate.ft_aggregate;
+
+        BytesReceived {
+            node_public_key: values[1].clone(),
+            era: values[3].clone(),
+            sum: values[5].parse::<u32>().expect("bytesReceivedSum must be convertable to u32"),
+        }
+    }
+}
+
+#[derive(Encode, Decode, Clone, Eq, PartialEq, Debug, TypeInfo, Default)]
+pub struct ValidationResult<BlockNumber, AccountId> {
+    block_number: BlockNumber,
+    signer: AccountId,
+    val_res: bool,
+    cdn_node_pub_key: String,
+}
+
+// use sp_std::fmt;
+// impl<T: frame_system::Config> fmt::Display for ValidationResult<T::BlockNumber, T::AccountId>
+// {
+// 	fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+// 		write!(f, "ValidationResult signer {}", self.signer)
+// 	}
+// }
+
+pub mod crypto {
+    use super::KEY_TYPE;
+    use frame_system::offchain::AppCrypto;
+    use sp_core::sr25519::Signature as Sr25519Signature;
+    use sp_runtime::{
+        app_crypto::{app_crypto, sr25519},
+        traits::Verify,
+    };
+    app_crypto!(sr25519, KEY_TYPE);
+
+    use sp_runtime::{MultiSignature, MultiSigner};
+
+    pub struct TestAuthId;
+
+    impl AppCrypto<<Sr25519Signature as Verify>::Signer, Sr25519Signature> for TestAuthId {
+        type RuntimeAppPublic = Public;
+        type GenericSignature = sp_core::sr25519::Signature;
+        type GenericPublic = sp_core::sr25519::Public;
+    }
+
+    impl AppCrypto<MultiSigner, MultiSignature> for TestAuthId {
+        type RuntimeAppPublic = Public;
+        type GenericSignature = sp_core::sr25519::Signature;
+        type GenericPublic = sp_core::sr25519::Public;
+    }
+}
+
+pub use pallet::*;
+
+#[frame_support::pallet]
+pub mod pallet {
+    use super::*;
+    use frame_support::pallet_prelude::*;
+    use frame_system::pallet_prelude::*;
+
+    #[pallet::pallet]
+    #[pallet::without_storage_info]
+    #[pallet::generate_store(pub(super) trait Store)]
+    pub struct Pallet<T>(_);
+
+    #[pallet::config]
+    pub trait Config:
+    frame_system::Config
+    + pallet_contracts::Config
+    + pallet_session::Config<ValidatorId = <Self as frame_system::Config>::AccountId>
+    + pallet_staking::Config
+    + CreateSignedTransaction<Call<Self>>
+        where
+            <Self as frame_system::Config>::AccountId: AsRef<[u8]> + UncheckedFrom<Self::Hash>,
+            <BalanceOf<Self> as HasCompact>::Type: Clone + Eq + PartialEq + Debug + TypeInfo + Encode,
+    {
+        /// The overarching dispatch call type.
+        type Call: From<Call<Self>>;
+
+        type AuthorityId: AppCrypto<Self::Public, Self::Signature>;
+    }
+
+    #[pallet::hooks]
+    impl<T: Config> Hooks<BlockNumberFor<T>> for Pallet<T>
+    where
+        <T as frame_system::Config>::AccountId: AsRef<[u8]> + UncheckedFrom<T::Hash>,
+        <BalanceOf<T> as HasCompact>::Type: Clone + Eq + PartialEq + Debug + TypeInfo + Encode,
+    {
+        fn offchain_worker(block_number: T::BlockNumber) {
+            let res = Self::offchain_worker_main(block_number);
+
+            match res {
+                Ok(()) => info!("[DDC Validator] DDC Validator is suspended."),
+                Err(err) => error!("[DDC Validator] Error in Offchain Worker: {}", err),
+            };
+        }
+    }
+
+    #[pallet::call]
+    impl<T: Config> Pallet<T>
+    where
+        <T as frame_system::Config>::AccountId: AsRef<[u8]> + UncheckedFrom<T::Hash>,
+        <BalanceOf<T> as HasCompact>::Type: Clone + Eq + PartialEq + Debug + TypeInfo + Encode,
+    {
+        #[pallet::weight(1000)]
+        pub fn save_validated_data(origin: OriginFor<T>, val_res: bool, cdn_node_pub_key: String, block_number: T::BlockNumber) -> DispatchResult {
+            let signer: T::AccountId = ensure_signed(origin)?;
+
+            info!("author: {:?}", signer);
+            let mut v_results = ValidationResults::<T>::get();
+
+            let cur_validation = ValidationResult::<T::BlockNumber, T::AccountId> {
+                block_number,
+                val_res,
+                cdn_node_pub_key,
+                signer,
+            };
+
+            v_results.push(cur_validation);
+
+            ValidationResults::<T>::set(v_results);
+
+            Ok(())
+        }
+    }
+
+    #[pallet::storage]
+    #[pallet::getter(fn validation_results)]
+    pub(super) type ValidationResults<T: Config> = StorageValue<_, Vec<ValidationResult::<T::BlockNumber, T::AccountId>>, ValueQuery>;
+}
+
+impl<T: Config> Pallet<T>
+where
+    <T as frame_system::Config>::AccountId: AsRef<[u8]> + UncheckedFrom<T::Hash>,
+    <BalanceOf<T> as HasCompact>::Type: Clone + Eq + PartialEq + Debug + TypeInfo + Encode,
+{
+    fn offchain_worker_main(block_number: T::BlockNumber) -> ResultStr<()> {
+        if block_number % ERA_IN_BLOCKS.into() != 0u32.into() {
+            return Ok(())
+        }
+
+        let signer = match Self::get_signer() {
+            Err(e) => {
+                warn!("{:?}", e);
+                return Ok(());
+            }
+            Ok(signer) => signer,
+        };
+
+        info!("ValidationResults: {:?}", ValidationResults::<T>::get());
+
+        // Read data from DataModel and do dumb validation
+        let current_era = Self::get_current_era();
+        let (bytes_sent, bytes_received) = Self::fetch_data(current_era);
+        let val_res = Self::validate(bytes_sent.clone(), bytes_received.clone());
+
+        let cdn_node_pub_key = bytes_sent.node_public_key.clone();
+        let tx_res = signer.send_signed_transaction(|_acct| {
+            info!("Sending save_validated_data tx");
+
+            // This is the on-chain function
+            Call::save_validated_data { val_res, cdn_node_pub_key: cdn_node_pub_key.clone(), block_number }
+        });
+
+        match &tx_res {
+            None | Some((_, Err(()))) => {
+                return Err("Error while submitting save_validated_data TX")
+            }
+            Some((_, Ok(()))) => {}
+        }
+
+        info!("save_validated_data: {:?}", ValidationResults::<T>::get());
+
+
+        Ok(())
+    }
+
+    fn get_signer() -> ResultStr<Signer<T, T::AuthorityId>> {
+        let signer = Signer::<_, _>::any_account();
+        if !signer.can_sign() {
+            return Err("[DDC Validator] No local accounts available. Consider adding one via `author_insertKey` RPC.");
+        }
+        Ok(signer)
+    }
+
+    fn validate(bytes_sent: BytesSent, bytes_received: BytesReceived) -> bool {
+        return if bytes_sent.sum == bytes_received.sum {
+            true
+        } else {
+            false
+        }
+    }
+
+    fn fetch_data(era: u64 ) -> (BytesSent, BytesReceived){
+        info!("[DDC Validator] DDC Validator is running. Current era is {}", era);
+        // Todo: handle the error
+        let bytes_sent_query = Self::get_bytes_sent_query_url(era);
+        let bytes_sent_res: RedisFtAggregate = Self::http_get_json(&bytes_sent_query).unwrap();
+        info!("Bytes sent sum is fetched: {:?}", bytes_sent_res);
+        let bytes_sent = BytesSent::new(bytes_sent_res);
+
+        // Todo: handle the error
+        let bytes_received_query = Self::get_bytes_received_query_url(era);
+        let bytes_received_res: RedisFtAggregate =
+            Self::http_get_json(&bytes_received_query).unwrap();
+        info!("Bytes received sum is fetched:: {:?}", bytes_received_res);
+        let bytes_received = BytesReceived::new(bytes_received_res);
+
+        (bytes_sent, bytes_received)
+    }
+
+    fn get_current_era() -> u64 {
+        // info!("timestamp: {}", timestamp().unix_millis());
+        // info!("TIME_START_MS: {}", TIME_START_MS);
+        // info!("ERA_DURATION_MS: {}", ERA_DURATION_MS);
+        // info!("1: {}", timestamp().unix_millis() - TIME_START_MS);
+        // info!("2: {}", (timestamp().unix_millis() - TIME_START_MS) % ERA_DURATION_MS);
+
+        (timestamp().unix_millis() - TIME_START_MS) / ERA_DURATION_MS
+    }
+
+    fn get_bytes_sent_query_url(era: u64) -> String {
+        format!("{}FT.AGGREGATE/ddc:dac:searchCommonIndex/@era:[{}%20{}]/GROUPBY/2/@nodePublicKey/@era/REDUCE/SUM/1/@bytesSent/AS/bytesSentSum", DATA_PROVIDER_URL, era, era)
+    }
+
+    fn get_bytes_received_query_url(era: u64) -> String {
+        format!("{}FT.AGGREGATE/ddc:dac:searchCommonIndex/@era:[{}%20{}]/GROUPBY/2/@nodePublicKey/@era/REDUCE/SUM/1/@bytesReceived/AS/bytesReceivedSum", DATA_PROVIDER_URL, era, era)
+    }
+
+    fn http_get_json<OUT: DeserializeOwned>(url: &str) -> ResultStr<OUT> {
+        let body = Self::http_get_request(url).map_err(|err| {
+            error!("[DDC Validator] Error while getting {}: {:?}", url, err);
+            "HTTP GET error"
+        })?;
+
+        let parsed = serde_json::from_slice(&body).map_err(|err| {
+            warn!("[DDC Validator] Error while parsing JSON from {}: {:?}", url, err);
+            "HTTP JSON parse error"
+        });
+
+        parsed
+    }
+
+    fn http_get_request(http_url: &str) -> Result<Vec<u8>, http::Error> {
+        info!("[DDC Validator] Sending request to: {:?}", http_url);
+
+        // Initiate an external HTTP GET request. This is using high-level wrappers from
+        // `sp_runtime`.
+        let request = http::Request::get(http_url);
+
+        let deadline = sp_io::offchain::timestamp().add(Duration::from_millis(HTTP_TIMEOUT_MS));
+
+        let pending = request.deadline(deadline).send().map_err(|_| http::Error::IoError)?;
+
+        let response = pending.try_wait(deadline).map_err(|_| http::Error::DeadlineReached)??;
+
+        if response.code != 200 {
+            warn!("[DDC Validator] http_get_request unexpected status code: {}", response.code);
+            return Err(http::Error::Unknown)
+        }
+
+        // Next we fully read the response body and collect it to a vector of bytes.
+        Ok(response.body().collect::<Vec<u8>>())
+    }
+
+    fn validators() -> Vec<<T as session::Config>::ValidatorId> {
+        <pallet_session::Pallet<T>>::validators()
+    }
+
+    /// Reward a validator.
+    pub fn reward_by_ids(
+        validators_points: impl IntoIterator<Item = (T::AccountId, u32)>,
+    ) -> Result<(), ()> {
+        <staking::Pallet<T>>::reward_by_ids(validators_points);
+        Ok(())
+    }
+}
