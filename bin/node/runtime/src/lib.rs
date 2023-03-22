495f40a1f08e7b13665193bd141bfbb54b3a549f
Raid Ateir <ateirraid@gmail.com>
2023-03-22 11:32:21 +0100

    merge offchain worker with ddc validator

--- bin/node/runtime/src/lib.rs
+++ bin/node/runtime/src/lib.rs
@@ -1243,6 +1243,8 @@ impl pallet_ddc_staking::Config for Runtime {
 impl pallet_ddc_validator::Config for Runtime {
 	type Event = Event;
 	type Randomness = RandomnessCollectiveFlip;
+	type Call = Call;
+	type AuthorityId = pallet_ddc_validator::crypto::TestAuthId;
 }
 
 construct_runtime!(
